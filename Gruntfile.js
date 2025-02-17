cbf6985414557b213c970200d368e73b42d1b91e
ProjectSoft <projectsoft2009@yandex.ru>
2025-02-18 00:31:17 +0400

    update

---
+++ Gruntfile.js
@@ -0,0 +1,151 @@
+module.exports = function(grunt) {
+	process.removeAllListeners('warning');
+
+	const fs = require("node:fs");
+
+	require('load-grunt-tasks')(grunt);
+
+	var pkg = grunt.file.readJSON('package.json');
+	//fs.readFileSync('tpl/component.html');
+	grunt.initConfig({
+		globalConfig: {},
+		pkg: pkg,
+		webfont: {
+			main: {
+				src: 'src/glyph/*.svg',
+				dest: 'src/font',
+				options: {
+					engine: 'node',
+					hashes: false,
+					destLess: 'src/less/fonts',
+					relativeFontPath: "/fonts/",
+					font: 'radioapp',
+					types: 'ttf',
+					fontFamilyName: 'Radio App',
+					stylesheets: ['less'],
+					syntax: 'bootstrap',
+					execMaxBuffer: 1024 * 400,
+					htmlDemo: false,
+					version: '1.0.0',
+					normalize: true,
+					startCodepoint: 0xE900,
+					iconsStyles: false,
+					autoHint: false,
+					templateOptions: {
+						baseClass: '',
+						classPrefix: 'icon-'
+					},
+					template: 'src/radioapp.less'
+				}
+			}
+		},
+		ttf2woff2: {
+			main: {
+				src: ["src/font/*"],
+				dest: "docs/fonts",
+			},
+		},
+		less: {
+			components: {
+				options: {
+					compress: false,
+					ieCompat: false,
+					plugins: [
+						
+					],
+					data: function(dest, src) {
+						return {
+							"hash": uniqid(),
+						}
+					}
+				},
+				files: {
+					'src/css/radiostation.css': [
+						'src/less/radiostation.less',
+					]
+				}
+			},
+		},
+		cssmin: {
+			options: {
+				mergeIntoShorthands: false,
+				roundingPrecision: -1
+			},
+			components: {
+				files: {
+					'src/css/radiostation.css': [
+						'src/css/radiostation.css'
+					]
+				}
+			},
+		},
+		pug: {
+			components: {
+				options: {
+					pretty: '',
+					separator: '',
+				},
+				files: [{
+					expand: true,
+					cwd: 'src/pug/components/',
+					src: '*.pug',
+					dest: 'src/test/',
+					filter: 'isFile',
+					ext: '.html',
+				}],
+			},
+		},
+		replace: {
+			components: {
+				options: {
+					patterns: [
+						{
+							match: /%template%/g,
+							replacement: fs.readFileSync('src/test/radiostation.html').toString()
+						}
+					]
+				},
+				files: [
+					{
+						expand: true,
+						flatten : true,
+						src: [
+							'src/js/components/radiostation.js'
+						],
+						dest: 'docs/js/',
+						filter: 'isFile'
+					},
+				]
+			},
+		},
+		uglify : {
+			options: {
+				ASCIIOnly: true,
+				compress: false,
+				//beautify: true
+			},
+			components: {
+				files: {
+					'docs/js/radiostation.js': [
+						'docs/js/radiostation.js',
+					],
+				},
+			},
+		},
+	});
+	grunt.registerTask(
+		'default',
+		[
+			// Font`s create && convert
+			"webfont",
+			"ttf2woff2",
+			// Components
+			"less:components",
+			"cssmin:components",
+			"pug:components",
+			"replace:components",
+			"uglify:components",
+			// Application
+		]
+	);
+}
---
+++ src/js/components/radiostation.js
@@ -0,0 +1,50 @@
+class RadioStation extends HTMLElement {
+	constructor() {
+		super();
+		let template = `%template%`;
+		this.attachShadow({
+			mode: "open"
+		}).innerHTML = template;
+	}
+
+	render() {
+		// Прорисовка
+	}
+
+	disconected() {
+		// Удаление объекта
+	}
+
+	static get observedAttributes() {
+		// Массив отслеживаемых атрибутов
+		return [];
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		// Отслеживание атрибутов
+		this.render();
+	}
+
+	handleError() {
+		// Ошибки?
+		console.log('ERROR');
+	}
+
+	connectedCallback() {
+		// Добавление в DOM
+		if(!this.rendered) {
+			this.render();
+			this.rendered = true;
+		}
+	}
+
+	disconnectedCallback() {
+		// Удаление из DOM.
+		if(this.rendered) {
+			this.rendered = false;
+			this.disconected();
+		}
+	}
+}
+
+customElements.define("radio-station", RadioStation);
